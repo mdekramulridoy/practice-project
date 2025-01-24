07b161490dcf01ba552e4a76d2a760797dfec7c5
ekramulhoqueridoy <158032414+ekramulhoqueridoy@users.noreply.github.com>
2025-01-24 18:32:28 +0600

    added login and logout

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,9 +1,2 @@
-import { createKindeAuthHandler } from '@kinde-oss/kinde-auth-nextjs';
-
-export const { GET, POST } = createKindeAuthHandler({
-  clientId: process.env.KINDE_CLIENT_ID,
-  clientSecret: process.env.KINDE_CLIENT_SECRET,
-  issuer: process.env.KINDE_ISSUER_URL,
-  redirectUri: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
-  logoutRedirectUri: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
-});
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
