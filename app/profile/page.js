a75d302ad15d48cedc0c079e093c76a909a70c15
ekramulhoqueridoy <158032414+ekramulhoqueridoy@users.noreply.github.com>
2025-01-24 19:39:32 +0600

    added protected route

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import { redirect } from "next/navigation"; // For redirecting unauthenticated users
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"; // Kinde session
 
-export default function page() {
+export default async function ProfilePage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  // If no user is logged in, redirect them to the login page
+  if (!user) {
+    redirect("/api/auth/login");
+  }
+
+  // Render the profile page for logged-in users
   return (
-    <div className='text-center py-6'>Welcome to your profile!</div>
-  )
+    <div className="text-center py-6">
+      <h1 className="text-2xl font-bold">Welcome to profile page!</h1>
+    </div>
+  );
 }
